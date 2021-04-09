a6339dc56aa87470880ea9fc82d8a28093427400
Vašek Lorenc <vaclav.lorenc@gmail.com>
2021-04-09 16:12:03 +0200

    Fixing findings from shellcheck, so that it passes tests.

--- download-cesky-rozhlas-junior.sh
+++ download-cesky-rozhlas-junior.sh
@@ -8,9 +8,9 @@
 URL="{$1}"
 
 # This ugly thing will turn the HTML page into an array of URLs & episode names
-content=$( curl -s ${URL} )
-items=$( echo ${content} | pup --charset utf-8 'div[class="sm2-playlist-wrapper"] a json{}' | jq -c '.[] | { href: .href, name: .children[].children[].text }' 2>/dev/null )
-item=$( echo ${content} | pup --charset utf-8 'div[class="sm2-playlist-wrapper"] a json{}' | jq -c '.[] | { href: .href, name: .text }' 2>/dev/null )
+content=$( curl -s "${URL}" )
+items=$( echo "${content}" | pup --charset utf-8 'div[class="sm2-playlist-wrapper"] a json{}' | jq -c '.[] | { href: .href, name: .children[].children[].text }' 2>/dev/null )
+item=$( echo "${content}" | pup --charset utf-8 'div[class="sm2-playlist-wrapper"] a json{}' | jq -c '.[] | { href: .href, name: .text }' 2>/dev/null )
 
 # If items are empty, we may be downloading from a page with a single file
 if [ -z "${items}" ]; then
@@ -26,9 +26,9 @@ fi
 # And now process it all
 while IFS= read -r line
 do
-  url="$(echo ${line}| jq -r '.href')"
+  url="$(echo """${line}"""| jq -r '.href')"
   # Neuter the name a bit, even though it could be better
-  name="$(echo ${line} | jq -r '.name' | tr ': .' '___')"
+  name="$(echo """${line}""" | jq -r '.name' | tr ': .' '___')"
   echo "Downloading to ${name}.mp3"
   curl -# "${url}" -o "${name}.mp3"
 done < <(printf '%s\n' "${items}")
