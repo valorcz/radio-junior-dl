ee993fb97b3823693c94ba7e162799d9ec9f81be
Safarik, Petr <psafarik@netsuite.com>
2021-04-18 12:22:07 +0200

    Do basic refactoring

--- download-cesky-rozhlas-junior.sh
+++ download-cesky-rozhlas-junior.sh
@@ -9,52 +9,104 @@ mandatoryApps=(pup jq)
 optionalApps=(id3tag)
 
 
-for APP in "${mandatoryApps[@]}"; do
-    if ( ! command -v "$APP" >/dev/null 2>&1 ); then
-        echo "ERROR: Application $APP not found" >&2
-        exit 1
+
+################################
+#
+# Widely used vars, list it here
+# as it'd default values
+#
+################################
+URL=''
+DEBUG=false
+#INTERACTIVE=false
+
+function verifyFunctions() {
+    local MANDATORY="$1"
+    shift
+    appList=("$@")
+    for APP in "${appList[@]}"; do
+        if ( ! command -v "$APP" >/dev/null 2>&1 ); then
+            if ( $MANDATORY ); then
+                echo "ERROR: Application $APP not found" >&2
+                exit 1
+            else
+                echo "WARNING: Application $APP not found, some features will not be provided" >&2
+            fi
+        fi
+    done
+}
+
+function debugPrint() {
+    if ( $DEBUG ); then
+        echo "$@"
     fi
-done
+}
 
-for APP in "${optionalApps[@]}"; do
-    if ( ! command -v "$APP" >/dev/null 2>&1 ); then
-        echo "WARNING: Application $APP not found, some features will not be provided" >&2
+function parseArgs() {
+    while [ $# -gt 0 ]; do
+        case $1 in
+            -h|--help) printHelp; exit; shift;;
+            -d|--debug|-v|--verb) DEBUG=true ; shift;;
+            #--interactive|-i) INTERACTIVE=true; shift ;;
+            *)  if [ $# -gt 1 ]; then
+                    echo "$0: error - unrecognized option $1. Use -h for help." >&2;
+                    exit 1
+                else
+                    URL="$1"
+                fi 
+                shift
+                ;;
+        esac
+    done
+}
+
+function fillValues() {
+    # This ugly thing will turn the HTML page into an array of URLs & episode names
+    content=$( curl -s "${URL}" )
+    items=$( echo "${content}" | pup --charset utf-8 'div[class="sm2-playlist-wrapper"] a json{}' | jq -c '.[] | { href: .href, name: .children[].children[].text }' 2>/dev/null )
+    item=$( echo "${content}" | pup --charset utf-8 'div[class="sm2-playlist-wrapper"] a json{}' | jq -c '.[] | { href: .href, name: .text }' 2>/dev/null )
+    description="$( echo "${content}" | pup --charset utf-8  'meta[name="description"]' json{} | jq -c '.[] | .content' )"
+
+    # If items are empty, we may be downloading from a page with a single file
+    if [ -z "${items}" ]; then
+        items="${item}"
     fi
-done
 
-# Just pass an Radio Junior URL
-URL="{$1}"
+    # If still empty, something is wrong
+    if [ -z "${items}" ]; then
+      echo "Nothing found; the script probably needs to be fixed." >&2
+      exit
+    fi
+}
 
-# This ugly thing will turn the HTML page into an array of URLs & episode names
-content=$( curl -s "${URL}" )
-items=$( echo "${content}" | pup --charset utf-8 'div[class="sm2-playlist-wrapper"] a json{}' | jq -c '.[] | { href: .href, name: .children[].children[].text }' 2>/dev/null )
-item=$( echo "${content}" | pup --charset utf-8 'div[class="sm2-playlist-wrapper"] a json{}' | jq -c '.[] | { href: .href, name: .text }' 2>/dev/null )
+function doDownload() {
+    while IFS= read -r line
+    do
+        url="$(echo """${line}"""| jq -r '.href')"
+        # Neuter the name a bit, even though it could be better
+        name="$(echo """${line}""" | jq -r '.name' | tr ': .' '___')"
+        #if the file exists and has a size greater than zero
+        if [ -s "${name}.mp3" ]; then
+            echo "${name} exists, skipping"
+            continue
+        fi
+        echo "Downloading to ${name}.mp3"
+        curl -# "${url}" -o "${name}.mp3"
+        id3tag -1 -2 --song="${name}" --desc="${description}" --album='Radio Junior' --genre=101 --artist="Radio Junior" --comment="${URL}" "${name}.mp3"
+    done < <(printf '%s\n' "${items}")
+}
 
-description="$( echo "${content}" | pup --charset utf-8  'meta[name="description"]' json{} | jq -c '.[] | .content' )"
 
-# If items are empty, we may be downloading from a page with a single file
-if [ -z "${items}" ]; then
-    items="${item}"
-fi
+function mainLoop() {
+    parseArgs "$@"
+    verifyFunctions "true"  "${mandatoryApps[@]}"
+    verifyFunctions "false" "${optionalApps[@]}"
+    
+    items=''
+    description=''
+    fillValues
 
-# If still empty, something is wrong
-if [ -z "${items}" ]; then
-  echo "Nothing found; the script probably needs to be fixed." >&2
-  exit
-fi
+    doDownload
+}
 
-# And now process it all
-while IFS= read -r line
-do
-  url="$(echo """${line}"""| jq -r '.href')"
-  # Neuter the name a bit, even though it could be better
-  name="$(echo """${line}""" | jq -r '.name' | tr ': .' '___')"
-  #if the file exists and has a size greater than zero
-  if [ -s "${name}.mp3" ]; then
-    continue
-  fi
-  echo "Downloading to ${name}.mp3"
-  curl -# "${url}" -o "${name}.mp3"
-  id3tag -1 -2 --song="${name}" --desc="${description}" --album='Radio Junior' --genre=101 --artist="Radio Junior" --comment="${URL}" "${name}.mp3"
-done < <(printf '%s\n' "${items}")
-
+mainLoop "$@"
